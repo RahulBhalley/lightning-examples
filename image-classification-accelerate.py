eae6372c45a6f79a0ac9fcbfa913ae095909998a
Rahul Bhalley <0behinds_secant@icloud.com>
2023-01-25 10:58:14 +0530

    Added Union[int, str] for precision

--- image-classification-accelerate.py
+++ image-classification-accelerate.py
@@ -11,6 +11,7 @@ from tqdm import tqdm
 import os
 from multiprocessing import cpu_count
 from argparse import ArgumentParser
+from typing import Union
 
 print(f"PyTorch version: {torch.__version__}")
 print(f"CPU cores: {cpu_count()}")
@@ -90,10 +91,10 @@ def get_opts():
     parser.add_argument('--accelerator', default='auto', type=str, help='Supports passing different accelerator types (“cpu”, “gpu”, “tpu”, “ipu”, “hpu”, “mps, “auto”) as well as custom accelerator instances')
     parser.add_argument('--devices', default=None, type=int, help='Will be mapped to either gpus, tpu_cores, num_processes or ipus, based on the accelerator type')
     parser.add_argument('--strategy', default=None, type=str, help='Strategy controls the model distribution across training, evaluation, and prediction to be used by the Trainer')
-    parser.add_argument('--precision', default=32, type=int, help='Double precision (64), full precision (32), half precision (16) or bfloat16 precision (bf16). Can be used on CPU, GPU, TPUs, HPUs or IPUs. Default: 32')
+    parser.add_argument('--precision', default=32, type=Union[int, str], help='Double precision (64), full precision (32), half precision (16) or bfloat16 precision (bf16). Can be used on CPU, GPU, TPUs, HPUs or IPUs. Default: 32')
     parser.add_argument('--accumulate_grad_batches', default=None, type=int, help='Accumulates grads every k batches or as set up in the dict. Default: None')
-    parser.add_argument('--batch_size', default=2, type=int, help='Batch size for training')
-    parser.add_argument("--image_size", default=32, type=int, help="The image size of dataset.")
+    parser.add_argument('--batch_size', default=16, type=int, help='Batch size for training')
+    parser.add_argument("--image_size", default=224, type=int, help="The image size of dataset.")
     parser.add_argument('--workers', default=cpu_count(), type=int, help='Number of train dataloader workers')
     parser.add_argument('--persistent_workers', default=False, action="store_true")
     parser.add_argument('--learning_rate', default=0.5, type=float, help='Optimizer learning rate')
